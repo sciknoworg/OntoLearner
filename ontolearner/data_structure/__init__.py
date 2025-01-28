cfc666bb5ccfa0c30dbee3d69907691a6619ea03
Hamed Babaei Giglou <hamedbabaeigiglou@gmail.com>
2025-01-28 20:37:37 +0100

    :pencil2: fix imports

--- ontolearner/data_structure/__init__.py
+++ ontolearner/data_structure/__init__.py
@@ -1,2 +1,2 @@
-from .data import TermTyping, TaxonomicRelation, NonTaxonomicRelation, OntologyData
-from .metric import TopologyMetrics, OntologyMetrics, DatasetMetrics, BenchmarkMetrics
+from .data import *
+from .metric import *
