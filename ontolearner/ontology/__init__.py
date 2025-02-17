8a89fd114abc4e652b736aa3e6856e642d70df84
aioaneia <andrei.c.aioanei@gmail.com>
2025-02-17 18:20:32 +0100

    :recycle: refactored industry and livestock ontologies

--- ontolearner/ontology/__init__.py
+++ ontolearner/ontology/__init__.py
@@ -1,11 +1,11 @@
 from .agricultural import * # NOQA
-from .animal_science import * # NOQA
+from .livestock import * # NOQA
 from .artwork import * # NOQA
 from .basic import * # NOQA
 from .biology import * # NOQA
 from .building_equipment import * # NOQA
 from .chemistry import * # NOQA
-from .enterprise import * # NOQA
+from .industry import * # NOQA
 from .environment import * # NOQA
 from .event import * # NOQA
 from .general import * # NOQA
---
+++ ontolearner/ontology/industry.py
@@ -0,0 +1,35 @@
+from ..base import BaseOntology
+
+
+class GIST(BaseOntology):
+    """
+    Gist is Semantic Arts' minimalist upper ontology for the enterprise.
+    It is designed to have the maximum coverage of typical business ontology concepts
+    with the fewest number of primitives and the least amount of ambiguity.
+
+    This class processes GIST ontology using default behavior.
+    """
+    ontology_full_name = "GIST Upper Ontology"
+
+
+class IOF(BaseOntology):
+    """
+    The IOF Core Ontology contains notions found to be common across multiple manufacturing domains.
+    This file is an RDF implementation of these notions. The ontology utilizes the Basic Formal Ontology or BFO
+    as a top-level ontology but also borrows terms from various domain-independent or mid-level ontologies.
+    The purpose of the ontology is to serve as a foundation for ensuring consistency
+    and interoperability across various domain-specific reference ontologies the IOF publishes.
+
+    This class processes the Industrial Ontology Foundry (IOF) ontology using default behavior.
+    """
+    ontology_full_name = "Industrial Ontology Foundry (IOF)"
+
+
+class DBO(BaseOntology):
+    """
+    The Digital Buildings ontology (DBO) is used by Google to represent structured information
+    about buildings and building-installed equipment.
+
+    This class processes DBO Ontology using default behavior.
+    """
+    ontology_full_name = "Digital Buildings Ontology (DBO)"
