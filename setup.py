d0920b2b0143851de308eb9a8654379e051686dd
Hamed Babaei Giglou <hamedbabaeigiglou@gmail.com>
2025-03-17 16:10:05 +0100

    :memo: added release changes

---
+++ setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+
+with open("README.md", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="OntoLearner",
+    version="0.1.0",
+    author="Hamed Babaei Giglou",
+    author_email="hamedbabaeigiglou@gmail.com",
+    description="OntoLearner: Ontology Learning Framework",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sciknoworg/OntoLearner",
+    packages=find_packages(),
+    install_requires=[
+        "rdflib==7.1.1",
+        "networkx==3.2.1",
+        "seaborn==0.13.2",
+        "numpy==1.26.4",
+        "pandas==2.0.3",
+        "matplotlib==3.9.4",
+        "tqdm==4.67.1",
+        "pydantic==2.10.5",
+        "pathlib==1.0.1",
+        "logging==0.4.9.6"
+    ],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.10,<4.0.0",
+    project_urls={
+        "Documentation": "https://ontolearner.readthedocs.io/",
+        "Source": "https://github.com/sciknoworg/OntoLearner",
+        "Tracker": "https://github.com/sciknoworg/OntoLearner/issues",
+    },
+)
