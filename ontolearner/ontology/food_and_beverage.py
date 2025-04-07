a04aa6741d45bd8bd592bb85bd4f731c7de2de33
aioaneia <andrei.c.aioanei@gmail.com>
2025-04-07 11:48:03 +0200

    :sparkles: added wine ontology, #206

---
+++ ontolearner/ontology/food_and_beverage.py
@@ -0,0 +1,8 @@
+from ..base import BaseOntology
+
+
+class Wine(BaseOntology):
+    """
+    A project to define an RDF style ontology for wines and the wine-industry
+    """
+    ontology_full_name = "Wine Ontology (Wine)"
