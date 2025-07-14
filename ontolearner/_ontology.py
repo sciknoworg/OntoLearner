6c0c8ac7c6f2e06137becd0a107d37001f2a3927
Hamed Babaei Giglou <hamedbabaeigiglou@gmail.com>
2025-07-14 13:53:56 +0200

    :memo: add docs

--- ontolearner/_ontology.py
+++ ontolearner/_ontology.py
@@ -17,6 +17,34 @@ import ontolearner.ontology as ontology_module
 from .base import BaseOntology
 
 class AutoOntology:
+    """
+       Factory class to automatically instantiate an ontology class from the `ontolearner.ontology` module
+       based on a given ontology ID.
+
+       This class scans all classes defined in the `ontolearner.ontology` module and returns an instance
+       of the first class that:
+         - has a callable `load` method,
+         - has an `ontology_id` attribute,
+         - and whose class name matches the given `ontology_id` (case-insensitive).
+
+       If no matching class is found, an instance of `BaseOntology` is returned by default.
+
+       Args:
+           ontology_id (str): The identifier (name) of the ontology class to instantiate.
+
+       Returns:
+           BaseOntology: An instance of the matching ontology class or a `BaseOntology` fallback.
+
+       Example:
+           >>> auto_onto = AutoOntology("AgrO")
+           >>> print(type(auto_onto))
+           <class 'ontolearner.ontology.AgrO'>
+
+           If no class matches "unknownontology":
+           >>> auto_onto = AutoOntology("unknownontology")
+           >>> print(type(auto_onto))
+           <class 'ontolearner.base.BaseOntology'>
+       """
     def __new__(self, ontology_id) -> BaseOntology:
         for name, obj in inspect.getmembers(ontology_module):
             if inspect.isclass(obj):
