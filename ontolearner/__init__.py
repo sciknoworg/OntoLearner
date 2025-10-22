a63f6b245aadb6e20fed4ed353a4f3a62cfdffa6
KrishnaRani <thakurkrishnarani@gmail.com>
2025-10-22 23:16:08 +0200

    add rwth_dbis learner models

--- ontolearner/__init__.py
+++ ontolearner/__init__.py
@@ -29,7 +29,9 @@ from .learner import (AutoLLMLearner,
                       AutoRetrieverLearner,
                       AutoRAGLearner,
                       StandardizedPrompting,
-                      LabelMapper)
+                      LabelMapper,
+                      RWTHDBISTaxonomyLearner,
+                      RWTHDBISTermTypingLearner)
 from ._learner import LearnerPipeline
 
 from .processor import Processor
@@ -47,6 +49,8 @@ __all__ = [
     "LabelMapper",
     "LearnerPipeline",
     "Processor",
+    "RWTHDBISTaxonomyLearner",
+    "RWTHDBISTermTypingLearner",
     "data_structure",
     "text2onto",
     "ontology",
